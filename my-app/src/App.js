9f3b02acbeb52d9066dbb805bbb394cfec76f152
pengying <pengy@golaxy.cn>
2018-05-17 18:56:07 +0800

    第一题

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+class HelloComponent extends React.Component {
+  render() {
+    return (
+      <div>
+          <h1>Hello world </h1>
+      </div>
+    );
+  }
+}
+
+export default HelloComponent ;
